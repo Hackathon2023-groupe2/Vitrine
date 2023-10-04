60b9d2e71fc236ca73f13d9eab4393ab175db261
goldenpeps <ewan4325@gmail.com>
2023-10-04 14:19:33 +0200

    feat: limite textarea

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 /**
  * Line count
- * @returns 
+ * @returns
  */
 function compterLignes() {
-  const textarea = document.getElementById('textareaId');
+  const textarea = document.getElementById("textareaId");
   if (!textarea) {
     console.error("Textarea non trouvé");
     return 0;
@@ -14,8 +14,8 @@ function compterLignes() {
   return lignes.length;
 }
 
-const textarea = document.getElementById('textareaId');
-const countArea = document.getElementById('countArea');
+const textarea = document.getElementById("textareaId");
+const countArea = document.getElementById("countArea");
 
 // Event for each input in the textarea
 textarea.addEventListener("input", (e) => {
@@ -24,7 +24,7 @@ textarea.addEventListener("input", (e) => {
   for (var i = 0; i < compterLignes(); i++) {
     let p = document.createElement("div");
     p.innerHTML = i + 1;
-    p.classList.add('flexBox')
+    p.classList.add("flexBox");
     countArea.appendChild(p);
   }
 });
@@ -41,21 +41,29 @@ countArea.addEventListener("scroll", () => {
 });
 
 function ajouterCaractere(Valeur) {
-  var maValeur = Valeur
+  var maValeur = Valeur;
   var textarea = document.getElementById("textareaId");
   textarea.value += maValeur;
 }
 function SupCaractere() {
- // Récupérer le contenu de la zone de texte
- var textArea = document.getElementById("textareaId");
- var texte = textArea.value;
+  var textArea = document.getElementById("textareaId");
+  var texte = textArea.value;
 
- // Vérifier si la zone de texte n'est pas vide
- if (texte.length > 0) {
-     // Supprimer le dernier caractère
-     texte = texte.substring(0, texte.length - 1);
+  if (texte.length > 0) {
+    texte = texte.substring(0, texte.length - 1);
 
-     // Mettre à jour le contenu de la zone de texte
-     textArea.value = texte;
- }
+    textArea.value = texte;
+  }
+}
+
+function limitCharacters(textarea) {
+  const lines = textarea.value.split("\n");
+
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].length > 20) {
+      lines[i] = lines[i].substring(0, 20);
+    }
+  }
+  
+  textarea.value = lines.join("\n");
 }
